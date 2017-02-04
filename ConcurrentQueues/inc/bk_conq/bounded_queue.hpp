cfd4652324c523a15e474c3c1d1cef53609136b4
Barath Kannan <Barathwaj.Kannan@gmail.com>
2017-02-04 14:00:41 +1100

    constrain interface

--- ConcurrentQueues/inc/bk_conq/bounded_queue.hpp
+++ ConcurrentQueues/inc/bk_conq/bounded_queue.hpp
@@ -9,10 +9,49 @@
 #define BK_CONQ_BOUNDEDQUEUE_HPP
 
 namespace bk_conq {
-class bounded_queue {
+
+class bounded_queue_tag {};
+
+template <typename T>
+class bounded_queue_typed_tag {};
+
+template <typename T, typename BASE>
+class bounded_queue : public bounded_queue_typed_tag<T>, public bounded_queue_tag {
 public:
-	virtual ~bounded_queue() {}
+	bool sp_enqueue(T&& input) {
+		return base()->sp_enqueue_impl(std::move(input));
+	}
+
+	bool sp_enqueue(const T& input) {
+		return base()->sp_enqueue_impl(input);
+	}
+
+	bool mp_enqueue(T&& input) {
+		return base()->mp_enqueue_impl(std::move(input));
+	}
+
+	bool mp_enqueue(const T& input) {
+		return base()->mp_enqueue_impl(input);
+	}
+
+	bool sc_dequeue(T& output) {
+		return base()->sc_dequeue_impl(output);
+	}
+
+	bool mc_dequeue(T& output) {
+		return base()->mc_dequeue_impl(output);
+	}
+
+	bool mc_dequeue_uncontended(T& output) {
+		return base()->mc_dequeue_uncontended_impl(output);
+	}
+
+private:
+	BASE* base() {
+		return static_cast<BASE*>(this);
+	}
 };
+
 }//namespace bk_conq
 
 #endif /* BK_CONQ_BOUNDEDQUEUE_HPP */
--- ConcurrentQueues/inc/bk_conq/unbounded_queue.hpp
+++ ConcurrentQueues/inc/bk_conq/unbounded_queue.hpp
@@ -1,7 +1,7 @@
 /*
  * File:   unbounded_queue.hpp
  * Author: Barath Kannan
- * Unbounded queue tag.
+ * Unbounded queue interface.
  * Created on 17 October 2016, 1:34 PM
  */
 
@@ -9,10 +9,49 @@
 #define BK_CONQ_UNBOUNDEDQUEUE_HPP
 
 namespace bk_conq {
-class unbounded_queue {
-public:
-	virtual ~unbounded_queue() {}
+
+class unbounded_queue_tag {};
+
+template <typename T>
+class unbounded_queue_typed_tag {};
+
+template <typename T, typename BASE>
+class unbounded_queue : public unbounded_queue_typed_tag<T>, public unbounded_queue_tag{
+public:	
+	void sp_enqueue(T&& input) {
+		base()->sp_enqueue_impl(std::move(input));
+	}
+
+	void sp_enqueue(const T& input) {
+		base()->sp_enqueue_impl(input);
+	}
+
+	void mp_enqueue(T&& input) {
+		base()->mp_enqueue_impl(std::move(input));
+	}
+
+	void mp_enqueue(const T& input) {
+		base()->mp_enqueue_impl(input);
+	}
+
+	bool sc_dequeue(T& output) {
+		return base()->sc_dequeue_impl(output);
+	}
+
+	bool mc_dequeue(T& output) {
+		return base()->mc_dequeue_impl(output);
+	}
+
+	bool mc_dequeue_uncontended(T& output) {
+		return base()->mc_dequeue_uncontended_impl(output);
+	}
+
+private:
+	BASE* base() {
+		return static_cast<BASE*>(this);
+	}
 };
+
 }//namespace bk_conq
 
 #endif /* BK_CONQ_UNBOUNDEDQUEUE_HPP */
