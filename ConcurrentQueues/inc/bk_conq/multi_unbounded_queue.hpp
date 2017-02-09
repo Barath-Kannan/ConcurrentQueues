3b0471a1725ddd522ca911ccc71b9e1e86c60187
Barath Kannan <Barathwaj.Kannan@gmail.com>
2017-02-09 23:19:43 +1100

    correct handling of thread local enqueue/dequeue heuristics across class instances

--- ConcurrentQueues/inc/bk_conq/multi_unbounded_queue.hpp
+++ ConcurrentQueues/inc/bk_conq/multi_unbounded_queue.hpp
@@ -15,18 +15,23 @@
 
 #include <thread>
 #include <vector>
+#include <mutex>
 #include <bk_conq/unbounded_queue.hpp>
 
 namespace bk_conq {
 
-template<typename TT, size_t N>
+template<typename TT, size_t N=32>
 class multi_unbounded_queue;
 
 template <template <typename> class Q, typename T, size_t N>
 class multi_unbounded_queue<Q<T>, N> : public unbounded_queue<T, multi_unbounded_queue<Q<T>, N>>{
 	friend unbounded_queue<T, multi_unbounded_queue<Q<T>, N>>;
 public:
-	multi_unbounded_queue(size_t subqueues) : _q(subqueues) {
+	multi_unbounded_queue(size_t subqueues) :
+		_q(subqueues),
+		_hitlist([&]() {return hitlist_sequence(); }),
+		_enqueue_identifier([&]() { return get_enqueue_index(); }, [&](size_t indx) {return return_enqueue_index(indx); })
+	{
 		static_assert(std::is_base_of<bk_conq::unbounded_queue_typed_tag<T>, Q<T>>::value, "Q<T> must be an unbounded queue");
 	}
 
@@ -36,18 +41,18 @@ public:
 protected:
 	template <typename R>
 	void sp_enqueue_impl(R&& input) {
-		thread_local size_t indx = _enqueue_indx.fetch_add(1) % _q.size();
+		size_t indx = _enqueue_identifier.get_tlcl();
 		_q[indx].sp_enqueue(std::forward<R>(input));
 	}
 
 	template <typename R>
 	void mp_enqueue_impl(R&& input) {
-		thread_local size_t indx = _enqueue_indx.fetch_add(1) % _q.size();
+		size_t indx = _enqueue_identifier.get_tlcl();
 		_q[indx].mp_enqueue(std::forward<R>(input));
 	}
 
 	bool sc_dequeue_impl(T& output) {
-		thread_local std::vector<size_t> hitlist = hitlist_sequence();
+		auto& hitlist = _hitlist.get_tlcl();
 		for (auto it = hitlist.begin(); it != hitlist.end(); ++it) {
 			if (_q[*it].sc_dequeue(output)) {
 				for (auto it2 = hitlist.begin(); it2 != it; ++it2) std::iter_swap(it, it2);
@@ -58,7 +63,7 @@ protected:
 	}
 
 	bool mc_dequeue_impl(T& output) {
-		thread_local std::vector<size_t> hitlist = hitlist_sequence();
+		auto& hitlist = _hitlist.get_tlcl();
 		for (auto it = hitlist.begin(); it != hitlist.end(); ++it) {
 			if (_q[*it].mc_dequeue_uncontended(output)) {
 				for (auto it2 = hitlist.begin(); it2 != it; ++it2) std::iter_swap(it, it2);
@@ -75,7 +80,7 @@ protected:
 	}
 
 	bool mc_dequeue_uncontended_impl(T& output) {
-		thread_local std::vector<size_t> hitlist = hitlist_sequence();
+		auto& hitlist = _hitlist.get_tlcl();
 		for (auto it = hitlist.begin(); it != hitlist.end(); ++it) {
 			if (_q[*it].mc_dequeue_uncontended(output)) {
 				for (auto it2 = hitlist.begin(); it2 != it; ++it2) std::iter_swap(it, it2);
@@ -86,10 +91,69 @@ protected:
 	}
 
 private:
-	inline std::vector<size_t> hitlist_sequence() {
+
+	template <typename U>
+	class tlcl {
+	public:
+		tlcl(std::function<U()> defaultvalfunc = nullptr, std::function<void(U)> defaultdeletefunc = nullptr) :
+			_defaultvalfunc(defaultvalfunc),
+			_defaultdeletefunc(defaultdeletefunc)
+		{
+			std::lock_guard<std::mutex> lock(_m);
+			if (!_available.empty()) {
+				_mycounter = _available.back();
+				_available.pop_back();
+			}
+			else {
+				_mycounter = _counter++;
+			}
+		}
+
+		virtual ~tlcl() {
+			std::lock_guard<std::mutex> lock(_m);
+			_available.push_back(_mycounter);
+		}
+
+		U& get_tlcl() {
+			thread_local std::vector<std::pair<tlcl<U>*, U>> vec;
+			if (vec.size() <= _mycounter) vec.resize(_mycounter+1);
+			auto& ret = vec[_mycounter];
+			if (ret.first != this) { //reset to default
+				if (_defaultdeletefunc && ret.first != nullptr) _defaultdeletefunc(ret.second);
+				if (_defaultvalfunc) ret.second = _defaultvalfunc();
+				ret.first = this;
+			}
+			return ret.second;
+		}
+
+	private:
+		std::function<U()> _defaultvalfunc;
+		std::function<void(U)> _defaultdeletefunc;
+		size_t _mycounter;
+		static size_t _counter;
+		static std::vector<size_t> _available;
+		static std::mutex _m;
+	};
+
+	std::vector<size_t> hitlist_sequence() {
 		std::vector<size_t> hitlist(_q.size());
 		std::iota(hitlist.begin(), hitlist.end(), 0);
 		return hitlist;
+	}
+
+	size_t get_enqueue_index() {
+		std::lock_guard<std::mutex> lock(_m);
+		if (_unused_enqueue_indexes.empty()) {
+			return _enqueue_index++;
+		}
+		size_t ret = _unused_enqueue_indexes.back();
+		_unused_enqueue_indexes.pop_back();
+		return ret;
+	}
+
+	void return_enqueue_index(size_t index) {
+		std::lock_guard<std::mutex> lock(_m);
+		_unused_enqueue_indexes.push_back(index);
 	}
 
 	class padded_unbounded_queue : public Q<T>{
@@ -97,8 +161,23 @@ private:
 	};
 
 	std::vector<padded_unbounded_queue> _q;
-	std::atomic<size_t> _enqueue_indx{ 0 };
+	
+	std::vector<size_t> _unused_enqueue_indexes;
+	size_t				_enqueue_index{ 0 };
+	std::mutex			_m;
+
+	tlcl<std::vector<size_t>> _hitlist;
+	tlcl<size_t> _enqueue_identifier;
 };
+
+template <template <typename> class Q, typename T, size_t N> template <typename U>
+size_t multi_unbounded_queue<Q<T>, N>::tlcl<U>::_counter = 0;
+
+template <template <typename> class Q, typename T, size_t N> template <typename U>
+std::vector<size_t> multi_unbounded_queue<Q<T>, N>::tlcl<U>::_available;
+
+template <template <typename> class Q, typename T, size_t N> template <typename U>
+std::mutex multi_unbounded_queue<Q<T>, N>::tlcl<U>::_m;
 
 }//namespace bk_conq
 
